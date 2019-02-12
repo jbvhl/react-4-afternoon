4b75fbc8c1217983135e8be99a5c0801f92f179a
Bao Le <jbvhl@hotmail.com>
2019-02-12 15:05:44 -0700

    donoo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <div className="box">
-        <button className='btn'>Math 1010</button>
-        <button className='btn'>English 2010</button>
-        <button className='btn'>Biology 2020</button>
+        <Link className='btn' to='/classlist/MATH1010'>Math 1010</Link>
+        <Link className='btn' to='/classlist/ENG2010'>English 2010</Link>
+        <Link className='btn' to='/classlist/BIO2020'>Biology 2020</Link>
       </div>
     );
   }
